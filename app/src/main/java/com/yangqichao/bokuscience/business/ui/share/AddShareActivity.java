dacb07a53b40f0574ee13e6d41e9f3fb5b63e357
yangqc_win\yangqc <793477679@qq.com>
2017-06-28 07:45:17 +0800

    123

--- app/src/main/java/com/yangqichao/bokuscience/business/ui/share/AddShareActivity.java
+++ app/src/main/java/com/yangqichao/bokuscience/business/ui/share/AddShareActivity.java
@@ -1,15 +1,59 @@
 package com.yangqichao.bokuscience.business.ui.share;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.yangqichao.bokuscience.R;
+import com.yangqichao.bokuscience.common.base.BaseActivity;
 
-public class AddShareActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AddShareActivity extends BaseActivity {
+
+
+    @BindView(R.id.et_h5)
+    EditText etH5;
+    @BindView(R.id.et_content)
+    EditText etContent;
+    @BindView(R.id.submit_staus)
+    TextView submitStaus;
+    @BindView(R.id.tv_file_name)
+    TextView tvFileName;
+    @BindView(R.id.ll_add_file)
+    LinearLayout llAddFile;
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_add_share;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_share);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.img_back, R.id.img_add, R.id.img_cancel})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.img_back:
+                finish();
+                break;
+            case R.id.img_add:
+                break;
+            case R.id.img_cancel:
+                break;
+        }
     }
 }
