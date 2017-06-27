fe3e47afe1626f0488d46add50d1e3d84ec2978d
杨旗超 <793477679@qq.com>
2017-06-28 07:16:48 +0800

    开始共建共享

---
+++ app/src/main/java/com/yangqichao/bokuscience/business/ui/share/AddShareActivity.java
@@ -0,0 +1,15 @@
+package com.yangqichao.bokuscience.business.ui.share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yangqichao.bokuscience.R;
+
+public class AddShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_share);
+    }
+}
--- app/src/main/java/com/yangqichao/bokuscience/business/ui/share/ShareActivity.java
+++ app/src/main/java/com/yangqichao/bokuscience/business/ui/share/ShareActivity.java
@@ -1,13 +1,23 @@
 package com.yangqichao.bokuscience.business.ui.share;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.yangqichao.bokuscience.R;
 import com.yangqichao.bokuscience.common.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class ShareActivity extends BaseActivity {
 
 
+    @BindView(R.id.recycle_share)
+    RecyclerView recycleShare;
+    @BindView(R.id.swipe)
+    SwipeRefreshLayout swipe;
 
     @Override
     protected int getLayoutResID() {
@@ -17,5 +27,18 @@ public class ShareActivity extends BaseActivity {
     @Override
     protected void initView(Bundle savedInstanceState) {
 
+    }
+
+
+    @OnClick({R.id.img_back, R.id.img_add})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.img_back:
+                finish();
+                break;
+            case R.id.img_add:
+
+                break;
+        }
     }
 }
